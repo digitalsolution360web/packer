a847c01273f5f03ec342eb9e634bfbb4f5f3886e
unknown <arpitkumar994@gmail.com>
2026-06-03 12:35:34 +0530

    Done

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,6 +44,48 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+   const schema = {
+  "@context": "https://schema.org",
+  "@type": "MovingCompany",
+  "name": "Packers and Movers Jalandhar",
+  "url": "https://www.packersandmoversjalandhar.com/",
+  "telephone": "+91-9855452352",
+  "alternateName": "Movers & Packers",
+  "email": "info@packersandmoversjalandhar.com",
+  "address": {
+    "@type": "PostalAddress",
+    "streetAddress": "150/2 Banda Bahadur Nagar, Near Ram Mandir, Sangat Colony",
+    "addressLocality": "Jalandhar",
+    "addressRegion": "Punjab",
+    "postalCode": "144001",
+    "addressCountry": "IN"
+  },
+  "openingHoursSpecification": {
+    "@type": "OpeningHoursSpecification",
+    "dayOfWeek": [
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+      "Sunday"
+    ],
+    "opens": "08:00",
+    "closes": "22:00"
+  },
+  "areaServed": [
+    {
+      "@type": "City",
+      "name": "Jalandhar"
+    },
+    {
+      "@type": "City",
+      "name": "Amritsar"
+    }
+  ],
+  "description": "Professional packing, moving, loading, unloading, household shifting, office relocation and transportation services in Jalandhar and nearby areas."
+}
   return (
     <html lang="en" className={`${inter.variable} ${outfit.variable} scroll-smooth`}>
       <head>
@@ -54,6 +96,12 @@ export default function RootLayout({
           crossOrigin="anonymous" 
           referrerPolicy="no-referrer" 
         />
+        <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(schema),
+        }}
+      />
       </head>
       <body className="antialiased bg-white text-slate-900 font-sans p-0 m-0 pb-16 lg:pb-0">
         <ModalProvider>
